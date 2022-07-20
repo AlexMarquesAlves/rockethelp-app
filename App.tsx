aefe812ce1198af6418f09b4a9102c3ba561d4ff
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-20 13:27:38 -0300

    Feat: Uding Fonts on App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
 import React from "react";
 
+import { Loading } from "./src/components";
 import { SignIn } from "./src/screen";
+
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
