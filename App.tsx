47182a69d022ffdb5566823b257982afa4bac9c4
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-20 12:51:39 -0300

    Feat: Added Theme styles on App

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import { NativeBaseProvider } from "native-base";
 import React from "react";
 
 import { SignIn } from "./src/screen";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SignIn />
     </NativeBaseProvider>
   );
