246628cad29a51492e136f3cf3305e97b04c8a5f
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-20 12:50:33 -0300

    Feat: Added Provider on App

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
+import { NativeBaseProvider } from "native-base";
 import React from "react";
-import { StyleSheet } from "react-native";
+
 import { SignIn } from "./src/screen";
 
 export default function App() {
-  return <SignIn />;
+  return (
+    <NativeBaseProvider>
+      <SignIn />
+    </NativeBaseProvider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/screen/SignIn/index.tsx
+++ src/screen/SignIn/index.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export const SignIn = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>SignIn</Text>
+      <Text style={styles.title}>Hello Duque</Text>
     </View>
   );
 };
