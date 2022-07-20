6a077bb9d420063aba0dacfef903b75818df88c8
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-20 13:42:13 -0300

    Feat: Added Logo

--- src/screen/SignIn/index.tsx
+++ src/screen/SignIn/index.tsx
@@ -1,8 +1,12 @@
 import { Heading, VStack } from "native-base";
 
+import Logo from "../../assets/logo_primary.svg";
+
 export const SignIn = () => {
   return (
     <VStack flex={1} alignItems={"center"} bg={"gray.600"} px={24}>
+      <Logo />
+
       <Heading color={"gray.100"} fontSize={"xl"} mt={20} mb={6}>
         Acesse sua conta
       </Heading>
