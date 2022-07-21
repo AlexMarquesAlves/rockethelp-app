f18640dd877642c57d24be450d578aa7cdd97cb0
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-21 13:22:58 -0300

    Feat: Created own type

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,5 +1,11 @@
-import { VStack } from "native-base";
+import { VStack, Text, Button, IButtonProps, useTheme } from "native-base";
 
-export function Filter() {
+type Props = IButtonProps & {
+  title: string;
+  isActive?: boolean;
+  type: "open" | "closed";
+};
+
+export function Filter({ title, isActive = false, type, ...rest }: Props) {
   return <VStack></VStack>;
 }
