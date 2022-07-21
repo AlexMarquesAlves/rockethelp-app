354a09d7b2196ae39594d21edfbd98402329daec
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-20 23:53:35 -0300

    Feat: Updated state on change text

--- src/screen/SignIn/index.tsx
+++ src/screen/SignIn/index.tsx
@@ -15,7 +15,7 @@ export const SignIn = () => {
       <Logo />
 
       <Heading color={"gray.100"} fontSize={"xl"} mt={20} mb={6}>
-        Acesse sua conta {name}
+        Acesse sua conta
       </Heading>
 
       <Input
@@ -24,12 +24,14 @@ export const SignIn = () => {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
       <Input
         mb={8}
         placeholder={"Senha"}
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
 
       <Button title={"Entrar"} w={"full"} />
