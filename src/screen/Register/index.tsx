70db04897702c69367e59d0776e2e6855af9c906
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-22 00:29:24 -0300

    Feat: Added first input

--- src/screen/Register/index.tsx
+++ src/screen/Register/index.tsx
@@ -1,10 +1,13 @@
 import { VStack } from "native-base";
-import { Header } from "../../components/Header";
+import { Button, Input } from "../../components";
+import { Header } from "../../components";
 
 export function Register() {
   return (
     <VStack flex={1} p={6} bg={"gray.600"}>
-      <Header title={"Nova Solicitação"} />
+      <Header title={"Nova solicitação"} />
+
+      <Input placeholder={"Número do patrimônio"} mt={4} />
     </VStack>
   );
 }
