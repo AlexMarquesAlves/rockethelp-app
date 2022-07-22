20fae89c12aff84f89f27bc10283101fcd78a962
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-22 00:34:07 -0300

    Feat: Updated Details VStack properties

--- src/screen/Details/index.tsx
+++ src/screen/Details/index.tsx
@@ -1,23 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { VStack } from "native-base";
+import { Header } from "../../components";
 
-export const Details = () => {
-  return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }>Details</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#312e38',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: '#fff',
-  },
-});
+export function Details() {
+  return <VStack flex={1} bg={"gray.700"}></VStack>;
+}
