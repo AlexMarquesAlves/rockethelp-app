e20296539bc0c28e3046f3272ab449b27d05c00e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-22 00:35:04 -0300

    Feat: Added Header component

--- src/screen/Details/index.tsx
+++ src/screen/Details/index.tsx
@@ -2,5 +2,9 @@ import { VStack } from "native-base";
 import { Header } from "../../components";
 
 export function Details() {
-  return <VStack flex={1} bg={"gray.700"}></VStack>;
+  return (
+    <VStack flex={1} bg={"gray.700"}>
+      <Header title={"Solicitação"} />
+    </VStack>
+  );
 }
