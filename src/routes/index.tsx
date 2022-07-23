85a53df11d3b0824cd395eeb20a4aa37b69a4aa9
¡Duque <atanaelleonardo@gmail.com>
2022-07-23 01:36:07 -0300

    Feat: Added new States to control is the user is logged

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { SignIn } from "../screens";
 import { AppRoutes } from "./app.routes";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { useState } from "react";
 
 export function Routes() {
+  const [loading, setLoading] = useState();
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
   return (
     <NavigationContainer>
       <AppRoutes />
