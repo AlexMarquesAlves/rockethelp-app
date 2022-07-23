f530d169bb3fd76e90107d9cf97e0d20ffa17819
¡Duque <atanaelleonardo@gmail.com>
2022-07-23 01:41:14 -0300

    Feat: Created useEffect to catch user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,23 @@ import { NavigationContainer } from "@react-navigation/native";
 import { SignIn } from "../screens";
 import { AppRoutes } from "./app.routes";
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Routes() {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setLoading(false);
+    });
+
+    return () => {
+      subscriber;
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <AppRoutes />
