79ef13a6ae72c846f54f16d431f2b8c5474124d9
¡Duque <atanaelleonardo@gmail.com>
2022-07-23 01:54:24 -0300

    Feat: Created three states

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,13 @@
 import { VStack } from "native-base";
+import { useState } from "react";
 import { Button, Input } from "../../components";
 import { Header } from "../../components";
 
 export function Register() {
+  const [isloading, setIsloading] = useState(false);
+  const [patrimony, setPatrimony] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <VStack flex={1} p={6} bg={"gray.600"}>
       <Header title={"Nova solicitação"} />
