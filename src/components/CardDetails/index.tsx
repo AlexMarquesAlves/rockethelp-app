9270249a66a9d3800d5c6653a6a51ebb9c5b48f3
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-25 01:27:08 -0300

    Feat: Added imports

--- src/components/CardDetails/index.tsx
+++ src/components/CardDetails/index.tsx
@@ -1,23 +1,7 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { VStack, HStack, Text, Box, useTheme } from "native-base";
+import { ReactNode } from "react";
+import { IconProps } from "phosphor-react-native";
 
-export const CardDetails = () => {
-  return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }>CardDetails</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#312e38',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: '#fff',
-  },
-});
+export function CardDetails() {
+  return <VStack></VStack>;
+}
